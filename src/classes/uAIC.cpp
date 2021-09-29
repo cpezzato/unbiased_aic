3423a00b58bf8d0b8a1c8c8c779d852de6a48ced
Corrado <corrado.pezzato@gmail.com>
2021-09-29 10:57:08 +0200

    commented unnecessary parts

--- src/classes/uAIC.cpp
+++ src/classes/uAIC.cpp
@@ -14,7 +14,7 @@
   // Constructor which takes as argument the publishers and initialises the private ones in the class
   uAIC::uAIC(int whichRobot){
 
-      // Initialize publishers on the topics /robot1/panda_joint*_controller/command for the joint efforts
+      // Torque publishers
       tauPub1 = nh.advertise<std_msgs::Float64>("/panda_joint1_controller/command", 20);
       tauPub2 = nh.advertise<std_msgs::Float64>("/panda_joint2_controller/command", 20);
       tauPub3 = nh.advertise<std_msgs::Float64>("/panda_joint3_controller/command", 20);
@@ -22,30 +22,28 @@
       tauPub5 = nh.advertise<std_msgs::Float64>("/panda_joint5_controller/command", 20);
       tauPub6 = nh.advertise<std_msgs::Float64>("/panda_joint6_controller/command", 20);
       tauPub7 = nh.advertise<std_msgs::Float64>("/panda_joint7_controller/command", 20);
+
+      // Listener joint states
       sensorSub = nh.subscribe("/joint_states", 1, &uAIC::jointStatesCallback, this);
 
       // Listener to goals
       goal_mu_dSub = nh.subscribe("/GoalPositions", 5, &uAIC::setGoalMuDCallback, this);
 
       // Publisher for the free-energy and sensory prediction errors
-      IFE_pub = nh.advertise<std_msgs::Float64>("panda_free_energy", 10);
-      SPE_pub = nh.advertise<std_msgs::Float64MultiArray>("panda_SPE", 10);
+      //IFE_pub = nh.advertise<std_msgs::Float64>("panda_free_energy", 10);
+      //SPE_pub = nh.advertise<std_msgs::Float64MultiArray>("panda_SPE", 10);
 
       // Publishers for beliefs
-      beliefs_mu_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu", 10);
-      beliefs_mu_p_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu_p", 10);
-      beliefs_mu_pp_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu_pp", 10);
-
-      // Publishers for beliefs
-      beliefs_mu_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu", 10);
-      beliefs_mu_p_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu_p", 10);
-      beliefs_mu_pp_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu_pp", 10);
+      //beliefs_mu_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu", 10);
+      //beliefs_mu_p_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu_p", 10);
+      //beliefs_mu_pp_pub = nh.advertise<std_msgs::Float64MultiArray>("beliefs_mu_pp", 10);
 
     // Initialize the variables for thr uAIC
     uAIC::initVariables();
   }
   uAIC::~uAIC(){}
 
+  // Method to set the current goal from topic, this is the input to the controller
   void uAIC::setGoalMuDCallback(const std_msgs::Float64MultiArray::ConstPtr& msg){
     for( int i = 0; i < 7; i++ ) {
       mu_d(i) = msg->data[i];
@@ -77,6 +75,17 @@
     // Support variable
     dataReceived = 0;
 
+    // Precision matrices (first set them to zero then populate the diagonal)
+    SigmaP_yq0 = Eigen::Matrix<double, 7, 7>::Zero();
+    SigmaP_yq1 = Eigen::Matrix<double, 7, 7>::Zero();
+    SigmaP_mu = Eigen::Matrix<double, 7, 7>::Zero();
+    SigmaP_muprime = Eigen::Matrix<double, 7, 7>::Zero();
+    K_p = Eigen::Matrix<double, 7, 7>::Zero();
+    K_d = Eigen::Matrix<double, 7, 7>::Zero();
+    K_i = Eigen::Matrix<double, 7, 7>::Zero();
+
+    // Begin Tuning parameters of u-AIC
+    //---------------------------------------------------------------
     // Variances associated with the beliefs and the sensory inputs
     var_mu = 5.0;
     var_muprime = 10.0;
@@ -91,18 +100,10 @@
 
     // Learning rates for the gradient descent (found that a ratio of 60 works good)
     k_mu = 11.67;
-    // k_a = 700;
+    // End tuning parameters
+    //---------------------------------------------------------------
 
-    // Precision matrices (first set them to zero then populate the diagonal)
-    SigmaP_yq0 = Eigen::Matrix<double, 7, 7>::Zero();
-    SigmaP_yq1 = Eigen::Matrix<double, 7, 7>::Zero();
-    SigmaP_mu = Eigen::Matrix<double, 7, 7>::Zero();
-    SigmaP_muprime = Eigen::Matrix<double, 7, 7>::Zero();
-    K_p = Eigen::Matrix<double, 7, 7>::Zero();
-    K_d = Eigen::Matrix<double, 7, 7>::Zero();
-    K_i = Eigen::Matrix<double, 7, 7>::Zero();
-
-
+    // Populate matrices
     for( int i = 0; i < SigmaP_yq0.rows(); i = i + 1 ) {
       SigmaP_yq0(i,i) = 1/var_q;
       SigmaP_yq1(i,i) = 1/var_qdot;
@@ -123,10 +124,10 @@
     h = 0.001;
 
     // Resize Float64MultiArray messages
-    uAIC_mu.data.resize(7);
-    uAIC_mu_p.data.resize(7);
-    uAIC_mu_pp.data.resize(7);
-    SPE.data.resize(2);
+    // uAIC_mu.data.resize(7);
+    // uAIC_mu_p.data.resize(7);
+    // uAIC_mu_pp.data.resize(7);
+    // SPE.data.resize(2);
   }
 
   void uAIC::minimiseF(){
@@ -138,9 +139,9 @@
     //SPEmu_pp = (mu_pp.transpose()+mu_p.transpose())*SigmaP_muprime*(mu_pp+mu_p);
 
     // Free-energy as a sum of squared values (i.e. sum the SPE)
-    F.data = SPEq + SPEdq + SPEmu_p + SPEmu_pp;
+    // F.data = SPEq + SPEdq + SPEmu_p + SPEmu_pp;
 
-    // Free-energy minimization using gradient descent and beliefs update
+    // AIC: Free-energy minimization using gradient descent and beliefs update
     //mu_dot = mu_p - k_mu*(-SigmaP_yq0*(jointPos-mu)+SigmaP_mu*(mu_p+mu-mu_d));
     //mu_dot_p = mu_pp - k_mu*(-SigmaP_yq1*(jointVel-mu_p)+SigmaP_mu*(mu_p+mu-mu_d)+SigmaP_muprime*(mu_pp+mu_p));
     //mu_dot_pp = - k_mu*(SigmaP_muprime*(mu_pp+mu_p));
@@ -161,34 +162,33 @@
     // Set curret values for next ieration
     I_gain = I_gain + mu_d-mu;
 
-
+    // Calculate and send control actions
+    uAIC::computeActions();
+    
     // Publish beliefs as Float64MultiArray
-    for (int i=0;i<7;i++){
-       uAIC_mu.data[i] = mu(i);
-       uAIC_mu_p.data[i] = mu_p(i);
-       //uAIC_mu_pp.data[i] = mu_pp(i);
-    }
+    // for (int i=0;i<7;i++){
+    //    uAIC_mu.data[i] = mu(i);
+    //    uAIC_mu_p.data[i] = mu_p(i);
+    //    //uAIC_mu_pp.data[i] = mu_pp(i);
+    // }
     // Define SPE message
     //SPE.data[0] = SPEq;
     //SPE.data[1] = SPEdq;
 
-    // Calculate and send control actions
-    uAIC::computeActions();
-
     // Publish free-energy
-    IFE_pub.publish(F);
+    //IFE_pub.publish(F);
 
     // Sensory prediction error publisher
     //SPE_pub.publish(SPE);
 
     // Publish beliefs
-    beliefs_mu_pub.publish(uAIC_mu);
-    beliefs_mu_p_pub.publish(uAIC_mu_p);
-    beliefs_mu_pp_pub.publish(uAIC_mu_pp);
+    // beliefs_mu_pub.publish(uAIC_mu);
+    // beliefs_mu_p_pub.publish(uAIC_mu_p);
+    // beliefs_mu_pp_pub.publish(uAIC_mu_pp);
   }
 
   void   uAIC::computeActions(){
-    // Compute control actions through gradient descent of F
+    // Compute control actions through gradient descent of F, AIC
     //u = u-h*k_a*(SigmaP_yq1*(jointVel-mu_p)+SigmaP_yq0*(jointPos-mu));
 
     // Unbiased uAIC
@@ -203,15 +203,16 @@
     tauPub7.publish(tau7);
   }
 
+  // Method to control if the joint states have been received already,
+  // used in the main function
   int uAIC::dataReady(){
-    // Method to control if the joint states have been received already,
-    // used in the main function
     if(dataReceived==1)
       return 1;
     else
       return 0;
   }
 
+  // Method to set the desired position from script, used in the main to initialize the arm to a central pose and keep it there
   void uAIC::setGoal(std::vector<double> desiredPos){
     for(int i=0; i<desiredPos.size(); i++){
       mu_d(i) = desiredPos[i];
