414efe17f337720b4ebee720ff7c569067547fc0
Corrado <corrado.pezzato@gmail.com>
2022-02-24 11:18:15 +0100

    added gripper control

--- scripts/point_to_point.py
+++ scripts/point_to_point.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
+import actionlib
 import numpy as np
 import time
-
+from franka_gripper import MoveAction, MoveGoal
 from unbiased_aic.msg import reference
 
 class TestController(object):
@@ -17,6 +18,12 @@ class TestController(object):
         self._action_msg = reference()
         self._action_msg.ref_position.data = np.array([-0.0, -0.917, -0.0, -2.619, 0.0349, 1.719, -0.0])
         self._action_msg.ref_velocity.data = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+        self.client = actionlib.SimpleActionClient('gripper_control', MoveAction)
+        self.client.wait_for_server()
+        print("Found grasp server")
+        # Creates a goal to send to the action server.
+        self.goal = MoveGoal()
 
     def run(self):
         t = 0.0
@@ -38,11 +45,23 @@ class TestController(object):
             t += 0.01
             self._action_pub.publish(self._action_msg)
             self._rate.sleep()
-
+    
+    def gripper_control(self, param):
+        if param == "grasp":
+            self.goal.width = 0.02
+            self.goal.speed = 0.03
+        else:
+            self.goal.width = 0.05
+            self.goal.speed = 0.03
+         # Send the goal to the action server.
+        self.client.send_goal(self.goal)
+         # Waits for the server to finish performing the action.
+        self.client.wait_for_result()
 
 if __name__ == "__main__":
     myTestController = TestController()
     try:
-        myTestController.run()
+        # myTestController.run()
+        myTestController.gripper_control("release")
     except rospy.ROSInterruptException:
         pass
